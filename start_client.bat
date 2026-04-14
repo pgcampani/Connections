1ffb91a8d69077b7337112bef0e5190da77c5bfe
pgcampani <pg.campani@gmail.com>
2026-04-14 15:23:25 +0200

    Rimossi file non necessari

--- start_client.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javac -cp "gson-2.13.2.jar" -d out client/*.java messages/*.java messages/requests/*.java messages/responses/*.java 
-java -cp "out;gson-2.13.2.jar" client.ClientMain
--- start_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javac -cp "gson-2.13.2.jar" -d out server/game/* server/*.java messages/*.java messages/responses/*.java messages/requests/*.java
-java -cp "out;gson-2.13.2.jar" server.ServerMain
